d5032c94ca6f0d561ee6a9bdac8a1d0bcdd6180a
Marcos Sabino <yan_sabino472013@hotmail.com>
2025-11-26 21:07:48 -0300

    Estruturando projeto

---
+++ script.js
@@ -0,0 +1,39 @@
+let cardContainer = document.querySelector(".card-container");
+let campoBusca = document.querySelector("header input");
+let dados = [];
+
+async function iniciarBusca() {
+    // Se os dados ainda não foram carregados, busca do JSON.
+    if (dados.length === 0) {
+        try {
+            let resposta = await fetch("drinks.json");
+            dados = await resposta.json();
+        } catch (error) {
+            console.error("Erro ao carregar os dados dos drink:", error);
+            return; // Interrompe a execução se houver erro
+        }
+    }
+
+    const termoBusca = campoBusca.value.toLowerCase();
+    const dadosFiltrados = dados.filter(dado => 
+        dado.nome.toLowerCase().includes(termoBusca) || 
+        dado.descricao.toLowerCase().includes(termoBusca)
+    );
+
+    renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+    cardContainer.innerHTML = ""; // Limpa os cards existentes antes de renderizar novos
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p>${dado.ano_primeira_aparicao}</p>
+        <p>${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `
+        cardContainer.appendChild(article);
+    }
+}
